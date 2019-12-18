ef45058befac12c50921294f54a73339c46af18d
moonhwan123 <58998949+moonhwan123@users.noreply.github.com>
2019-12-18 09:20:40 +0900

    upadte1217

---
+++ Day_1216/src/Exam_04.java
@@ -0,0 +1,46 @@
+/*
+ 
+ *
+ **
+ ***
+ ****
+ *****
+ 
+ 
+ */
+public class Exam_04 {
+	
+	public static void main(String[] args) {
+		
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.print("*");
+		System.out.println();
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ Day_1217/src/Exam_05.java
@@ -0,0 +1,12 @@
+
+public class Exam_05 {
+	
+	public static void main(String[] args) throws Exception{
+		
+		System.out.print("1문자 입력 하세요 : ");
+		int var = System.in.read();
+		
+		System.out.println("출력 : " + (char)var); // 형변환
+	}
+
+}
