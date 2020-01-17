25ada4c36aed0b2089503ac13b556a314c387e23
moonhwan123 <58998949+moonhwan123@users.noreply.github.com>
2020-01-17 18:02:08 +0900

    update0117_final

--- Day_0117/src/sawon2/SawonData.java
+++ Day_0117/src/sawon2/SawonData.java
@@ -5,6 +5,7 @@ public class SawonData {
 	int cnt;
 	boolean sw;
 	int total;
+	int payTotal;
 	
 	SawonData(){}
 	SawonData(Sawon[] sa , int cnt){
@@ -15,6 +16,7 @@ public class SawonData {
 	
 	
 	void totalPrint() {
+		total=sa[0].getPay();
 		System.out.println("** 급여 관리 프로그램 **");
 		System.out.print("부서명\t사번\t이름\t급여\n");
 		System.out.println("-----------------------------");
@@ -29,14 +31,20 @@ public class SawonData {
 			if(sa[x].getBuseo().equals(sa[x-1].getBuseo())) {
 				System.out.print("\t"+sa[x].getSabun()+"\t");
 				System.out.print(sa[x].getName()+"\t"+sa[x].getPay()+"\n");
+				total+=sa[x].getPay();
 				
 			}else {
 				System.out.println("합계\t\t\t"+total);
+				total=sa[x].getPay();
 				System.out.print(sa[x].getBuseo()+"\t"+sa[x].getSabun()+"\t");
 				System.out.print(sa[x].getName()+"\t"+sa[x].getPay()+"\n");
+				
 			}	
+			payTotal +=  sa[x].getPay();
 		}
 		System.out.println("합계\t\t\t"+total);
+		System.out.println("총 합계\t\t\t"+(payTotal+sa[0].getPay()));
+		
 		
 
 		
